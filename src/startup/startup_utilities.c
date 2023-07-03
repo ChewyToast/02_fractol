f642a607d1a89dd2eadbeddee975988926afff52
Bruno <103060102+ChewyToast@users.noreply.github.com>
2023-07-03 17:26:17 +0200

    fix: syntax

--- src/startup/startup_utilities.c
+++ src/startup/startup_utilities.c
@@ -91,7 +91,7 @@ size_t	error_msg(int mode)
 	ft_printf("\nEnter the desired fractal as follows:");
 	ft_printf("\n⚜️  For \033[4mMandelbrot\033[0m set type: mandelbrot.");
 	ft_printf("\n⚜️  For \033[4mJulia\033[0m set type: julia;");
-	ft_printf(" or julia \033[3m(x value) (i value)\033[0m.");
+	ft_printf(" or julia \033[3m(x value) (y value)\033[0m.");
 	ft_printf("\n⚜️  For \033[4mTricorn\033[0m set type: tricorn");
 	ft_printf("\n⚜️  For \033[4mBurning\033[0m Ship set type: burningship");
 	ft_printf("\n\nRemember that the Julia set is only drawn ");
